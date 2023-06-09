28b0a925b2ba3e025d41154c23a420122cf202a6
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-06-09 17:03:55 -0400

    adding style to advice card

--- src/app/Advice.js
+++ src/app/Advice.js
@@ -1,9 +1,19 @@
+import Image from 'next/image'
+import page from './page.module.css'
 export default function AdviceCard() {
     return (
-        <>
-            <div className="card">
-                <p>Advice n° </p>
-           </div>
-        </>
+        <div className={page.background}>
+            <div className='card'>
+                <h2 className={page.numberadvice} >Advice n° </h2>
+                <h3 className={page.Advice} >Here is your advide: </h3>
+                <div className='divider'></div>
+                <div className={page.dicedivider}>
+                    <Image src={require('.//images/icon-dice.svg')} alt='dice-divider'
+                        width
+
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
