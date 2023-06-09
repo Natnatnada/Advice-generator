f820781474defaea2fbde229fdddedcd3632d9c7
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-06-09 18:52:44 -0400

    fetch API

--- src/app/Advice.js
+++ src/app/Advice.js
@@ -1,15 +1,36 @@
+'use client'; //add to avoid error import server components inside client 
 import Image from 'next/image'
 import page from './page.module.css'
+import { useState, useEffect } from 'react'
 export default function AdviceCard() {
+    //set quote to and empty array
+    const [quote, setQuote] = useState("")
+    const [numberQuote, setNumberQuote] = useState("")
+    // [Advice Slip API](https://api.adviceslip.com)
+    useEffect(() => {
+        //check the url, before the error was /advices
+        fetch('https://api.adviceslip.com/advice')
+            .then(response => response.json())
+            .then(
+                (quote) => {
+                    // console.log(quote)
+                    setQuote(quote.slip.advice);
+                    setNumberQuote(quote.slip.id)
+                })
+            .catch(error => {
+                console.log(error);
+            });
+    })
+    
     return (
         <div className={page.background}>
             <div className='card'>
-                <h2 className={page.numberadvice} >Advice n° </h2>
-                <h3 className={page.Advice} >Here is your advide: </h3>
-                <div className='divider'></div>
+                <h2 className={page.numberadvice} >ADVICE N° {numberQuote} </h2>
+                <h3 className={page.Advice} > {quote}</h3>
+                <div className={page.divider}></div>
                 <div className={page.dicedivider}>
                     <Image src={require('.//images/icon-dice.svg')} alt='dice-divider'
-                        width
+
 
                     />
                 </div>
